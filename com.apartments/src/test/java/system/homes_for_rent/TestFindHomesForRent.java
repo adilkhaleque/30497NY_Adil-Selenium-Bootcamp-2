8d0594f3c4ff43fe56f4ec1e6b2022298ff706c5
adilkhaleque <adilkhaleque429@gmail.com>
2022-12-03 22:36:11 -0500

    test cases 8, 9, 10 passed

--- com.apartments/src/test/java/system/homes_for_rent/TestFindHomesForRent.java
+++ com.apartments/src/test/java/system/homes_for_rent/TestFindHomesForRent.java
@@ -1,6 +1,24 @@
 package system.homes_for_rent;
 
+import apartments_page_library.HomePage;
+import apartments_page_library.SearchResultsPage;
 import base.BasePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class TestFindHomesForRent extends BasePage {
+
+    @Test(dataProviderClass = data_providers.DataProviders.class, dataProvider = "testLAHomesForRent")
+    public void testFindLAHomesForRent(String locationIndex, String text) {
+        HomePage homePage = new HomePage();
+        homePage.clickOnMenuButton();
+        homePage.clickOnHomesForRent();
+
+        SearchResultsPage searchResultsPage = homePage.selectRentalHomeLocation(Integer.parseInt(locationIndex));
+        searchResultsPage.chooseAmenitiesForRental();
+        searchResultsPage.clickOnSortButton();
+        searchResultsPage.clickOnRentSortOption();
+
+        Assert.assertEquals(searchResultsPage.getForRentLocationText(), text);
+    }
 }
