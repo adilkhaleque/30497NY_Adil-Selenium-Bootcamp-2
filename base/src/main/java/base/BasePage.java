11d8e65067c0a804bed8970baba3dcab195fc396
adilkhaleque <adilkhaleque429@gmail.com>
2022-12-27 22:38:03 -0500

    updates

--- base/src/main/java/base/BasePage.java
+++ base/src/main/java/base/BasePage.java
@@ -37,7 +37,7 @@ public class BasePage {
 
     Map<Object, String> dbConfig = BaseConfig.databaseConfig();
     public static final String DATA_PATH = System.getProperty("user.dir") + File.separator + "src" + File.separator
-            + "test" + File.separator + "resources" + File.separator + "test_data_parabank.xlsx";
+            + "test" + File.separator + "resources" + File.separator + "test_data_apartments.xlsx";
     public static ExcelData excel;
     public static Database db;
     public static WebDriver driver;
@@ -85,7 +85,7 @@ public class BasePage {
 
     @Parameters({"driverConfigEnabled", "browser", "url"})
     @BeforeMethod
-    public void driverSetup(@Optional("true") String driverConfigEnabled, @Optional("chrome") String browser, @Optional("https://parabank.parasoft.com") String url) {
+    public void driverSetup(@Optional("true") String driverConfigEnabled, @Optional("chrome") String browser, @Optional("https://apartments.com") String url) {
         if (Boolean.parseBoolean(driverConfigEnabled)) {
             driverInit(browser);
             driver.get(url);
